4a53f9f2f47ad69313ac0cd976b1e3e0f4a6e737
Alberte Egholm <alberteegholm@Doomeds-MacBook-Pro.local>
2018-10-31 15:18:23 +0100

    titelskærm og startknap

--- script.js
+++ script.js
@@ -30,20 +30,22 @@ function hideStart() {
     document.querySelector("#play").classList.remove("pulse");
     document.querySelector("#start").classList.add("fade_out");
 
-        document.querySelector("#start").addEventListener("animationend", StartGame);
+    document.querySelector("#start").addEventListener("animationend", StartGame);
 
 }
 
-function startGame (){
+function startGame() {
 
 
-  console.log("start game");
+    console.log("start game");
 
 
     document.querySelector("#start").removeEventListener("animationend", StartGame);
-     document.querySelector("#start").classList.add("fade_out");
+    document.querySelector("#start").classList.add("fade_out");
 
 
+    document.querySelector("#start").classList.add("hide");
 
-        document.querySelector("#start").classList.add("hide");
+
 }
+
